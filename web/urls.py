f3bc37f72397d62e14cae80b7bb1a851213ed182
MagisterFelix <felix.vlad.14@gmail.com>
2021-10-26 02:31:34 +0300

    Change links

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import UserRegistrationView, UserLoginView, UserProfileView
 
 urlpatterns = [
-    url(r'api/signUp', UserRegistrationView.as_view(), name='signUp'),
-    url(r'api/signIn', UserLoginView.as_view(), name='signIn'),
-    url(r'api/profile', UserProfileView.as_view(), name='profile'),
+    url(r'signUp/', UserRegistrationView.as_view()),
+    url(r'signIn/', UserLoginView.as_view()),
+    url(r'profile/', UserProfileView.as_view()),
 ]
