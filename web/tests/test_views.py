2c161b53af6a67e331053971e5c418db9df696f0
AlexeyTrofimenko <byalexmail@gmail.com>
2021-10-26 20:53:15 +0300

    Add task management tests

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 from rest_framework.exceptions import ErrorDetail
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, force_authenticate
 
-from web.tests import future
-from web.views import UserRegistrationView, UserLoginView, UserProfileView
-from web.models import User, Topic
+from web.views import UserRegistrationView, UserLoginView, UserProfileView, TopicView, TaskView, CommentView
+from web.models import User, Topic, Task, Comment
 
 
 class AuthViewTest(TestCase):
@@ -123,22 +122,35 @@ class AuthViewTest(TestCase):
                              {'detail': ErrorDetail(string='User account is disabled.', code='authentication_failed')})
 
 
-@future
 class TopicViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Topic.objects.create(name=f"Static_Topic", desc=f"Static_Description")
+        strong_password = '53175bcc0524f37b47062faf5da28e3f8eb91d51'
+        admin_mail = 'admin@gmail.com'
+        user_mail = 'default@gmail.com'
+
+        User.objects.create_superuser(
+            email=admin_mail, password=strong_password)
+
+        User.objects.create_user(
+            email=user_mail, password=strong_password, name='User', birthday='2000-12-13')
+
+        Topic.objects.create(name=f'Static_Topic', desc=f'Static_Description')
 
     def setUp(self):
         self.factory = APIRequestFactory()
-        Topic.objects.create(name=f"Dynamic_Topic", desc=f"Dynamic_Description")
+        self.admin = User.objects.get(is_superuser=True)
+        self.user = User.objects.get(is_superuser=False)
+        Topic.objects.create(name=f'Dynamic_Topic', desc=f'Dynamic_Description')
 
     def test_topic_create(self):
         topics_before = Topic.objects.count()
-        request = self.factory.post(path='', data={'name': 'New_Topic',
-                                                   'desc': 'New_Description', }, format='json')
-        response = TopicCreateView.as_view()(request)
+
+        request = self.factory.post(path='topic', data={'name': 'New_Topic',
+                                                        'desc': 'New_Description', }, format='json')
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': True, 'status code': 201, 'message': 'Topic created successfully.'})
         topics_after = Topic.objects.count()
@@ -146,9 +158,10 @@ class TopicViewTest(TestCase):
 
     def test_topic_create_if_exists(self):
         topics_before = Topic.objects.count()
-        request = self.factory.post(path='', data={'name': 'Static_Topic',
-                                                   'desc': 'Static_Description', }, format='json')
-        response = TopicCreateView.as_view()(request)
+        request = self.factory.post(path='topic', data={'name': 'Static_Topic',
+                                                        'desc': 'Static_Description', }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': False, 'status code': 400, 'message': 'Topic with this name already exists.'})
         topics_after = Topic.objects.count()
@@ -156,8 +169,9 @@ class TopicViewTest(TestCase):
 
     def test_topic_delete(self):
         topics_before = Topic.objects.count()
-        request = self.factory.post(path='', data={'id': 2, }, format='json')
-        response = TopicDeleteView.as_view()(request)
+        request = self.factory.delete(path='topic', data={'name': 'Dynamic_Topic', }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': True, 'status code': 200, 'message': 'Topic deleted successfully.'})
         topics_after = Topic.objects.count()
@@ -165,18 +179,20 @@ class TopicViewTest(TestCase):
 
     def test_topic_delete_if_not_exists(self):
         topics_before = Topic.objects.count()
-        request = self.factory.post(path='', data={'id': 1000, }, format='json')
-        response = TopicDeleteView.as_view()(request)
+        request = self.factory.delete(path='topic', data={'name': 'Unreal_Topic', }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
-                             {'success': False, 'status code': 404, 'message': 'Topic does not exists.'})
+                             {'success': False, 'status code': 404, 'message': 'Topic does not exist.'})
         topics_after = Topic.objects.count()
         self.assertEquals(topics_before, topics_after)
 
     def test_topic_update(self):
-        request = self.factory.post(path='',
-                                    data={'id': 2, 'name': 'New_Dynamic_Topic', 'desc': 'New_Dynamic_Desc'},
-                                    format='json')
-        response = TopicUpdateView.as_view()(request)
+        request = self.factory.put(path='topic',
+                                   data={'id': 2, 'name': 'New_Dynamic_Topic', 'desc': 'New_Dynamic_Desc'},
+                                   format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': True, 'status code': 200, 'message': 'Topic updated successfully.'})
 
@@ -184,9 +200,172 @@ class TopicViewTest(TestCase):
         self.assertListEqual([t.name, t.desc], ['New_Dynamic_Topic', 'New_Dynamic_Desc'])
 
     def test_topic_update_if_not_exists(self):
-        request = self.factory.post(path='',
-                                    data={'id': 1000, 'name': 'New_Dynamic_Topic', 'desc': 'New_Dynamic_Desc'},
-                                    format='json')
-        response = TopicUpdateView.as_view()(request)
+        request = self.factory.put(path='topic',
+                                   data={'id': 1000, 'name': 'New_Dynamic_Topic', 'desc': 'New_Dynamic_Desc'},
+                                   format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TopicView.as_view()(request)
         self.assertDictEqual(response.data,
-                             {'success': False, 'status code': 404, 'message': 'Topic does not exists.'})
+                             {'success': False, 'status code': 404, 'message': 'Topic does not exist.'})
+
+
+class TaskViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        strong_password = '53175bcc0524f37b47062faf5da28e3f8eb91d51'
+        admin_mail = 'admin@gmail.com'
+        user_mail = 'default@gmail.com'
+
+        User.objects.create_superuser(
+            email=admin_mail, password=strong_password)
+
+        User.objects.create_user(
+            email=user_mail, password=strong_password, name='User', birthday='2000-12-13')
+
+        Topic.objects.create(name='stat_topic', desc='')
+        topic = Topic.objects.get(id=1)
+        Task.objects.create(name='stat_task', desc='', complexity=0, topic=topic, input='', output='', solution='')
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.admin = User.objects.get(is_superuser=True)
+        self.user = User.objects.get(is_superuser=False)
+        self.topic = Topic.objects.get(id=1)
+        Task.objects.create(name='dyn_task', desc='', complexity=0, topic=self.topic, input='', output='', solution='')
+
+    def test_task_create(self):
+        tasks_before = Task.objects.count()
+        request = self.factory.post(path='task',
+                                    data={'name': 'New_Task', 'desc': '', 'complexity': 0, 'topic': self.topic.name,
+                                          'input': '', 'output': '', 'solution': ''},
+                                    format='json')
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': True, 'status code': 201, 'message': 'Task created successfully.'})
+        tasks_after = Task.objects.count()
+        self.assertLess(tasks_before, tasks_after)
+
+    def test_task_create_if_exists(self):
+        tasks_before = Task.objects.count()
+        request = self.factory.post(path='task',
+                                    data={'name': 'dyn_task', 'desc': '', 'complexity': 0, 'topic': self.topic.name,
+                                          'input': '', 'output': '', 'solution': ''},
+                                    format='json')
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': False, 'status code': 400, 'message': 'Task with this name already exists.'})
+        tasks_after = Task.objects.count()
+        self.assertEquals(tasks_before, tasks_after)
+
+    def test_task_delete(self):
+        tasks_before = Task.objects.count()
+        request = self.factory.delete(path='task', data={'name': 'dyn_task', }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': True, 'status code': 200, 'message': 'Task deleted successfully.'})
+        tasks_after = Task.objects.count()
+        self.assertGreater(tasks_before, tasks_after)
+
+    def test_task_delete_if_not_exists(self):
+        tasks_before = Task.objects.count()
+        request = self.factory.delete(path='task', data={'name': 'unreal_task', }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': False, 'status code': 404, 'message': 'Task does not exist.'})
+        tasks_after = Task.objects.count()
+        self.assertEquals(tasks_before, tasks_after)
+
+    def test_task_update(self):
+        request = self.factory.put(path='task',
+                                   data={'id': 2, 'name': 'new', 'desc': 'new', 'complexity': 1,
+                                         'topic': self.topic.name,
+                                         'input': 'new', 'output': 'new', 'solution': 'new'},
+                                   format='json')
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': True, 'status code': 200, 'message': 'Task updated successfully.'})
+
+        t = Task.objects.get(id=2)
+        t_dict = t.__dict__
+        t_dict.pop('_state')
+
+        self.assertDictEqual(t_dict,
+                             {'id': 2, 'name': 'new', 'desc': 'new', 'complexity': 1, 'topic_id': 1, 'input': 'new',
+                              'output': 'new', 'solution': 'new', })
+
+    def test_task_update_if_not_exists(self):
+        request = self.factory.put(path='task',
+                                   data={'id': 1000, 'name': 'new', 'desc': 'new', 'complexity': 1,
+                                         'topic': self.topic.name,
+                                         'input': 'new', 'output': 'new', 'solution': 'new'},
+                                   format='json')
+        force_authenticate(request, user=self.admin)
+        response = TaskView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': False, 'status code': 404, 'message': 'Task does not exist.'})
+
+
+class CommentViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        strong_password = '53175bcc0524f37b47062faf5da28e3f8eb91d51'
+        admin_mail = 'admin@gmail.com'
+        user_mail = 'default@gmail.com'
+
+        User.objects.create_superuser(
+            email=admin_mail, password=strong_password)
+
+        User.objects.create_user(
+            email=user_mail, password=strong_password, name='User', birthday='2000-12-13')
+        user = User.objects.get(id=1)
+        Topic.objects.create(name='stat_topic', desc='')
+        topic = Topic.objects.get(id=1)
+        Task.objects.create(name='stat_task', desc='', complexity=0, topic=topic, input='', output='', solution='')
+        task = Task.objects.get(id=1)
+        Comment.objects.create(user=user, task=task, message='static_msg')
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.admin = User.objects.get(is_superuser=True)
+        self.user = User.objects.get(is_superuser=False)
+        self.task = Task.objects.get(id=1)
+        Comment.objects.create(user=self.user, task=self.task, message='dyn_msg')
+
+    def test_comment_create(self):
+        comments_before = Task.objects.count()
+        request = self.factory.post(path='comment',
+                                    data={'task': 'stat_task', 'user': self.admin.name, 'message': 'msg'},
+                                    format='json')
+        force_authenticate(request, user=self.admin)
+        response = CommentView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': True, 'status code': 201, 'message': 'Comment created successfully.'})
+        comments_after = Comment.objects.count()
+        self.assertLess(comments_before, comments_after)
+
+    def test_comment_delete(self):
+        comments_before = Comment.objects.count()
+        request = self.factory.delete(path='comment', data={'id': 2, }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = CommentView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': True, 'status code': 200, 'message': 'Comment deleted successfully.'})
+        comments_after = Comment.objects.count()
+        self.assertGreater(comments_before, comments_after)
+
+    def test_comment_delete_if_not_exists(self):
+        comments_before = Comment.objects.count()
+        request = self.factory.delete(path='comment', data={'id': 1000, }, format='json', )
+        force_authenticate(request, user=self.admin)
+        response = CommentView.as_view()(request)
+        self.assertDictEqual(response.data,
+                             {'success': False, 'status code': 404, 'message': 'Comment does not exist.'})
+        comments_after = Comment.objects.count()
+        self.assertEquals(comments_before, comments_after)
