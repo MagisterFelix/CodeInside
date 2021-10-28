532960bc218310936f95019aab63c67d097215ab
AlexeyTrofimenko <byalexmail@gmail.com>
2021-10-28 17:43:10 +0300

    Add time zone handling

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -20,27 +20,29 @@ class AuthViewTest(TestCase):
         self.strong_password = "53175bcc0524f37b47062faf5da28e3f8eb91d51"
 
     def test_registration(self):
-        request = self.factory.post(path='/api/signUp', data={'email': 'test@gmail.com',
-                                                              'password': self.strong_password,
-                                                              'name': 'User',
-                                                              'birthday': '10/10/2000'}, format='json')
+        request = self.factory.post(path='signUp', data={'email': 'test@gmail.com',
+                                                         'password': self.strong_password,
+                                                         'name': 'User',
+                                                         'birthday': '10/10/2000',
+                                                         'time_zone': 'UTC', }, format='json')
         response = UserRegistrationView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': True, 'status code': 201, 'message': 'User registered successfully.'})
 
     def test_registration_if_user_already_exists(self):
-        request = self.factory.post(path='/api/signUp',
+        request = self.factory.post(path='signUp',
                                     data={'email': 'default@gmail.com',
                                           'password': self.strong_password,
                                           'name': 'User',
-                                          'birthday': '10/10/2000'}, format='json')
+                                          'birthday': '10/10/2000',
+                                          'time_zone': 'UTC', }, format='json')
 
         response = UserRegistrationView.as_view()(request)
         self.assertDictEqual(response.data,
                              {'success': False, 'status code': 400, 'message': 'User with this email already exists.'})
 
     def test_login(self):
-        request = self.factory.post(path='/api/signIn',
+        request = self.factory.post(path='signIn',
                                     data={'email': 'default@gmail.com',
                                           'password': self.strong_password, }, format='json')
 
@@ -50,7 +52,7 @@ class AuthViewTest(TestCase):
         self.assertEquals(len(response.data['token']), 200)
 
     def test_login_if_user_not_exists(self):
-        request = self.factory.post(path='/api/signIn',
+        request = self.factory.post(path='signIn',
                                     data={'email': 'idk@gmail.com',
                                           'password': self.strong_password, }, format='json')
         response = UserLoginView.as_view()(request)
@@ -61,13 +63,13 @@ class AuthViewTest(TestCase):
 
     def test_login_with_token(self):
         email = 'default@gmail.com'
-        request = self.factory.post(path='/api/signIn',
+        request = self.factory.post(path='signIn',
                                     data={'email': email,
                                           'password': self.strong_password, }, format='json')
 
         response = UserLoginView.as_view()(request)
         token = response.data['token']
-        token_request = self.factory.get(path='/api/profile',
+        token_request = self.factory.get(path='profile',
                                          format='json',
                                          HTTP_AUTHORIZATION=f'Bearer {token}', )
 
@@ -79,7 +81,7 @@ class AuthViewTest(TestCase):
 
     def test_login_with_wrong_token(self):
         wrong_token = "0" * 200
-        request = self.factory.get(path='/api/profile',
+        request = self.factory.get(path='profile',
                                    format='json',
                                    HTTP_AUTHORIZATION=f'Bearer {wrong_token}', )
 
@@ -92,7 +94,7 @@ class AuthViewTest(TestCase):
         u.is_active = False
         u.save()
 
-        request = self.factory.post(path='/api/signIn',
+        request = self.factory.post(path='signIn',
                                     data={'email': 'default@gmail.com',
                                           'password': self.strong_password, }, format='json')
 
@@ -103,7 +105,7 @@ class AuthViewTest(TestCase):
 
     def test_login_with_token_if_user_banned(self):
         email = 'default@gmail.com'
-        request = self.factory.post(path='/api/signIn',
+        request = self.factory.post(path='signIn',
                                     data={'email': email,
                                           'password': self.strong_password, }, format='json')
 
@@ -114,7 +116,7 @@ class AuthViewTest(TestCase):
         u.is_active = False
         u.save()
 
-        token_request = self.factory.get(path='/api/profile',
+        token_request = self.factory.get(path='profile',
                                          format='json',
                                          HTTP_AUTHORIZATION=f'Bearer {token}', )
         token_response = UserProfileView.as_view()(token_request)
