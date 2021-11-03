67735e399c73a19724208d528393155d5b0a419a
AlexeyTrofimenko <oleksii.trofimenko@nure.ua>
2021-11-03 10:13:18 +0200

    Handle achievement image link

--- web/models/achievement.py
+++ web/models/achievement.py
@@ -4,6 +4,7 @@ from django.db import models
 class Achievement(models.Model):
     name = models.CharField(max_length=50, unique=True)
     desc = models.CharField(max_length=200, blank=True)
+    link = models.CharField(max_length=100, blank=True)
     discount = models.IntegerField(default=0)
 
     class Meta:
